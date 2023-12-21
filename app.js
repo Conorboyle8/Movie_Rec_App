96487cebbe896487e74d44493a7e2844e6101e25
Conorboyle8 <69359717+Conorboyle8@users.noreply.github.com>
2023-12-20 21:12:52 -0500

    Update app.js       -search imp

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-var genresBtn = document.getElementById("genresBtn");
-var genresDropdown = document.getElementById("genresDropdown");
+function searchMovies() {
+                // Get the input value
+                var searchTerm = document.getElementById("search").value.toLowerCase();
 
-// Add a click event listener to the button
-genresBtn.addEventListener("click", function() {        // this tells the java script to listen and know when to use its function
-    // Toggle the visibility of the dropdown content
-    if (genresDropdown.style.display === "" || genresDropdown.style.display === "none") {
-        genresDropdown.style.display = "block";
-    } else {
-        genresDropdown.style.display = "none";
-    }
-});
+                // Movie data (replace with your actual movie data)
+                var movieData = [
+                    { title: "The Shawshank Redemption", genre: "Drama" },
+                    { title: "The Godfather", genre: "Crime" },
+                    { title: "Inception", genre: "Sci-Fi" },
+                    { title: "Titanic", genre: "Romance" },
+                    { title: "Avatar", genre: "Action" }
+                ];
+
+                // Perform the search logic
+                var results = [];
+
+                for (var i = 0; i < movieData.length; i++) {
+                    if (movieData[i].title.toLowerCase().includes(searchTerm)) {
+                        results.push(movieData[i]);
+                    }
+                }
+
+                // Display the search results
+                displaySearchResults(results);
+            }
+
+            function displaySearchResults(results) {
+                var resultContainer = document.createElement("div");
+                resultContainer.classList.add("search-results");
+
+                if (results.length === 0) {
+                    resultContainer.textContent = "No results found.";
+                } else {
+                    for (var i = 0; i < results.length; i++) {
+                        var resultItem = document.createElement("p");
+                        resultItem.textContent = results[i].title + " - Genre: " + results[i].genre;
+                        resultContainer.appendChild(resultItem);
+                    }
+                }
+
+                // Replace or append the search results to your HTML
+                var contentDiv = document.querySelector(".content");
+                contentDiv.appendChild(resultContainer);
+            }
